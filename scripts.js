06a7b2b9c8284a190c1f6c3161d602fcbd90c169
David Carvajal Garrido <david.carvajal@thebridgeschool.es>
2020-05-12 18:00:26 +0200

    Fix exercises content

--- scripts.js
+++ scripts.js
@@ -1,103 +1,66 @@
-class Painter {
-    constructor() {
-        this.createGallery()
-    }
-
-    createGallery() {
-        const $gallery = document.createElement('section')
-        const $body = document.querySelector('body')
-
-        $body.appendChild($gallery)
-        this.$gallery = $gallery
-    }
-
-    createImageTag(imageUrl) {
-        const $picture = document.createElement('picture')
-        const $img = document.createElement('img')
-
-        $img.setAttribute('src', imageUrl)
-        $picture.appendChild($img)
-
-        return $picture
-    }
-
-    paintSingleImage(imageUrl) {
-        const $imageTag = this.createImageTag(imageUrl)
-        this.$gallery.append($imageTag)
-    }
-
-    paintMultipleImages(arrayOfImages) {
-        for (const image of arrayOfImages) {
-            const $imageTag = this.createImageTag(image)
-            this.$gallery.append($imageTag)
-        }
-    }
-}
-
-
 const civilAircrafts = [
     'assets/avion-civil-1.jpg',
     'assets/avion-civil-2.jpg',
     'assets/avion-civil-3.jpg',
     'assets/avion-civil-4.jpg',
-    'assets/avion-civil-5.jpg',
-]
+    'assets/avion-civil-5.jpg'
+];
 
 const militaryAircrafts = [
     'assets/avion-militar-1.jpg',
     'assets/avion-militar-2.jpg',
     'assets/avion-militar-3.jpg',
     'assets/avion-militar-4.jpg',
-    'assets/avion-militar-5.jpg',
-]
+    'assets/avion-militar-5.jpg'
+];
 
 const civilHelicopters = [
     'assets/helicoptero-civil-1.jpg',
     'assets/helicoptero-civil-2.jpg',
     'assets/helicoptero-civil-3.jpg',
     'assets/helicoptero-civil-4.jpg',
-    'assets/helicoptero-civil-5.jpg',
-]
+    'assets/helicoptero-civil-5.jpg'
+];
 
 const militaryHelicopter = [
     'assets/helicoptero-militar-1.jpg',
     'assets/helicoptero-militar-2.jpg',
     'assets/helicoptero-militar-3.jpg',
     'assets/helicoptero-militar-4.jpg',
-    'assets/helicoptero-militar-5.jpg',
-]
+    'assets/helicoptero-militar-5.jpg'
+];
 
 class Gallery {
     constructor(civilImages, militaryImages) {
-        this.civilImages = civilImages
-        this.militaryImages = militaryImages
     }
     
     getRandomCivil() {
-        return this.civilImages[Math.floor(Math.random() * this.civilImages.length)]
     }
     
     getRandomMilitary() {
-        return this.militaryImages[Math.floor(Math.random() * this.civilImages.length)]
     }
     
     getAll() {
-        const allImages = []
-        for (const image of this.civilImages) {
-            allImages.push(image)
-        }
-        for (const image of this.militaryImages) {
-            allImages.push(image)
-        }
-        return allImages
     }
 }
 
-const aircrafts = new Gallery(civilAircrafts, militaryAircrafts)
-const helicopters = new Gallery(civilHelicopters, militaryHelicopter)
+class Painter {
+    constructor() {
+    }
 
-console.log(aircrafts, helicopters)
+    createGallery() {
+    }
 
-const painter = new Painter()
-// painter.paintSingleImage(aircrafts.getRandomCivil())
-// painter.paintMultipleImages(helicopters.getAll())
+    createImageTag(imageUrl) {
+    }
+
+    paintSingleImage(imageUrl) {
+    }
+
+    paintMultipleImages(arrayOfImages) {
+    }
+}
+
+const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
+const helicopters = new Gallery(civilHelicopters, militaryHelicopter);
+const painter = new Painter();
